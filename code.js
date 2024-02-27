817561f528443d93447d5c08f7f0bfbcb50b13e4
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-02-26 18:50:56 -0700

    Update code.js

--- code.js
+++ code.js
@@ -2,12 +2,12 @@ function mergesort(array) {
     const n = array.length;
     for (let width = 1; width < n; width = 2 * width) {
         for (let i = 0; i < n; i = i + 2 * width) {
-            // Defining the bounds of the subarray to merge
+            // Defining the bounds of the subarray to merge.
             const left = i;
             const right = Math.min(i + 2 * width - 1, n - 1);
             const mid = Math.min(i + width - 1, n - 1);
 
-            // Merge the subarrays in place
+            // Merge the subarrays in place.
             inPlaceMerge(array, left, mid, right);
         }
     }
@@ -18,19 +18,19 @@ function inPlaceMerge(array, left, mid, right) {
     let i = left;
     let j = mid + 1;
 
-    // Shifting smaller element from right subarray into correct position in left subarray.
+    // Shifting smaller element from right subarray into correct position in left subarray..
     while (i <= mid && j <= right) {
         if (array[i] <= array[j]) {
             i++;
         } else {
-            // Element from the right subarray is smaller and needs to be inserted before the left subarray element
+            // Element from the right subarray is smaller and needs to be inserted before the left subarray element.
             let value = array[j];
             for (let k = j; k > i; k--) {
                 array[k] = array[k - 1];
             }
             array[i] = value;
 
-            // Adjusting indices since we've inserted the element from the right subarray into the correct position
+            // Adjusting indices since we've inserted the element from the right subarray into the correct position.
             i++;
             mid++;
             j++;
